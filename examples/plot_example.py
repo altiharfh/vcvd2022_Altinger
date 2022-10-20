2aa642d83b6ecc63f6ce3b2ec147883456cd64f4
Altinger Harald <altinger.harald@gmail.com>
2022-10-20 15:23:15 +0200

    adopted name to match google code guidelines

---
+++ examples/plot_example.py
@@ -0,0 +1,37 @@
+__doc__ = "sample file for ploting method \
+           baed on \
+           https://www.geeksforgeeks.org/matplotlib-figure-figure-add_axes-in-python/"
+
+#based on
+#source: https://www.geeksforgeeks.org/matplotlib-figure-figure-add_axes-in-python/
+
+#required imports
+from cmath import pi
+import matplotlib.pyplot as plt
+import numpy as np
+
+#================================================
+def exec_sample_plot_(file_name_out):
+  exec_sample_plot_.__doc__ = "sample call to mathplotlib"
+
+  #define figure
+  fig = plt.figure()
+  #add one plot
+  ax1 = fig.add_subplot(111)
+
+  #data
+  t = np.arange(0.0, 1.0, 0.01)
+  s = np.sin(2 * np.pi * t)
+  #define plots
+  line, = ax1.plot(t, s, color ="green", lw = 2)
+
+  #add axis label
+  ax1.set_xlabel("time")
+  ax1.set_ylabel("sin (2 pi t)")
+
+  #add plot label
+  fig.suptitle("Plot Sample\n\n", fontweight ="bold")
+
+  #export as PDF
+  plt.savefig(file_name_out)
+  
