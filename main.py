05eef288f79972621216a777c0661f228c6a8dce
Altinger Harald <altinger.harald@gmail.com>
2023-12-09 09:16:04 +0100

    add secound parameter

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ print("Hello world\n")
 #setup arg parser
 arg_parser_ = argparse.ArgumentParser(description="Process some integers.")
 arg_parser_.add_argument("pdf_file_out", type=str, help="filename to plot")
+arg_parser_.add_argument("sec", type=str, help="sec_argument")
 cmd_call_args_ = arg_parser_.parse_args()
 print (cmd_call_args_.pdf_file_out)
 
